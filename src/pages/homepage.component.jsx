1c18ff31b87f6dad33b1ae25f0716c85e01746af
abpandya-at-376864425070 <abpandya@deloitte.com>
2020-02-07 16:41:09 -0500

    Broken into components for HomePage

---
+++ src/pages/homepage.component.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './homepage.styles.scss'
+import Directory from '../components/directory/directory.component'
+const HomePage=()=>(
+
+<div className="home-page">
+<div className="directory-menu">
+<Directory ></Directory>
+</div>
+    </div>
+)
+export default HomePage 
